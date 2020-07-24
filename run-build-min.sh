2193b2a838ec59bc9e821d8d808c48a2ed97c52d
Robert Wenzel <robertwenzel@potix.com>
2020-07-24 14:08:48 +0800

    upgrade elm to 0.19.1

--- run-build-min.sh
+++ run-build-min.sh
@@ -8,7 +8,7 @@ min=build/main.min.js
 
 elm make --optimize --output=$out $src
 
-uglifyjs $out --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | uglifyjs --mangle --output=$min
+npx uglifyjs $out --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe" | npx uglifyjs --mangle --output $min
 
 echo "Initial size:  $(cat $out | wc -c) bytes  ($out)"
 echo "Minified size: $(cat $min | wc -c) bytes  ($min)"
